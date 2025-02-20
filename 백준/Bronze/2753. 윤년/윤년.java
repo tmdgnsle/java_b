e244b3a4077e4a6d8a3d7345ef3b80993fd218c2
seunghoonlee <65535314+tmdgnsle@users.noreply.github.com>
2025-02-20 11:30:41 +0900

    [Bronze V] Title: 윤년, Time: 104 ms, Memory: 14164 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int year = Integer.parseInt(br.readLine());
+        
+        int result;
+        
+        if((year % 4 == 0) && ((year % 100 != 0) || (year % 400 == 0))){
+            result = 1;
+        } else {
+            result = 0;
+        }
+
+        System.out.println(result);
+    }
+}
