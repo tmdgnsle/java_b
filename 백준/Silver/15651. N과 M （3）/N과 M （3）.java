69fde2c680deb4e90ef8edcf52d5da9209457d08
seunghoonlee <65535314+tmdgnsle@users.noreply.github.com>
2025-02-26 15:27:58 +0900

    [Silver III] Title: N과 M (3), Time: 564 ms, Memory: 262080 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,36 @@
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static int N, M;
+	static ArrayList<Integer> result;
+	static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		M = sc.nextInt();
+
+		result = new ArrayList<Integer>();
+
+		bt();
+        System.out.println(sb);
+	}
+
+	static void bt() {
+		if (result.size() == M) {
+			for (int i = 0; i < M; i++) {
+				sb.append(result.get(i)).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+
+		for (int i = 1; i <= N; i++) {
+			result.add(i);
+			bt();
+			result.remove(result.size() - 1);
+		}
+	}
+}
