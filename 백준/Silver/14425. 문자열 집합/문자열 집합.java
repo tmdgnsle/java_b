65dfce3179f7a0986432e167255dd95bc877df0f
seunghoonlee <65535314+tmdgnsle@users.noreply.github.com>
2025-03-07 08:54:08 +0900

    [Silver IV] Title: 문자열 집합, Time: 2676 ms, Memory: 868384 KB -BaekjoonHub

---
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		tNode root = new tNode();
+		while (n > 0) {
+			String text = sc.next();
+			tNode now = root;
+			for (int i = 0; i < text.length(); i++) {
+				char c = text.charAt(i);
+				if (now.next[c - 'a'] == null) {
+					now.next[c - 'a'] = new tNode();
+				}
+				now = now.next[c - 'a'];
+				if (i == text.length() - 1)
+					now.isEnd = true;
+			}
+			n--;
+		}
+		int count = 0;
+		while (m > 0) {
+			String text = sc.next();
+			tNode now = root;
+			for (int i = 0; i < text.length(); i++) {
+				char c = text.charAt(i);
+				if (now.next[c - 'a'] == null)
+					break;
+				now = now.next[c - 'a'];
+				if (i == text.length() - 1 && now.isEnd)
+					count++;
+			}
+			m--;
+		}
+		System.out.println(count);
+	}
+
+}
+
+class tNode {
+	tNode[] next = new tNode[26];
+	boolean isEnd;
+}
