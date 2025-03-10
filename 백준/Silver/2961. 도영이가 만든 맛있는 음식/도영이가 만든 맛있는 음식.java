a185388f1c21deee9b555c3472b5d80e03e146da
seunghoonlee <65535314+tmdgnsle@users.noreply.github.com>
2025-03-10 17:30:39 +0900

    [Silver II] Title: 도영이가 만든 맛있는 음식, Time: 68 ms, Memory: 11680 KB -BaekjoonHub

---
+++ 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.java
@@ -0,0 +1,70 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static int N;
+	static int result;
+	static boolean[] v;
+	static Taste[] ig;
+	static ArrayList<Taste> arr;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		ig = new Taste[N];
+		v = new boolean[N];
+		result = Integer.MAX_VALUE;
+		for (int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int s = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+
+			ig[i] = new Taste(s, b);
+		}
+
+		arr = new ArrayList<>();
+
+		bt(0);
+
+		System.out.println(result);
+	}
+
+	static void bt(int depth) {
+		if (!arr.isEmpty()) {
+			int S = 1;
+			int B = 0;
+
+			for (Taste t : arr) {
+				S *= t.s;
+				B += t.b;
+			}
+
+			int temp = Math.abs(S - B);
+			result = Math.min(result, temp);
+
+		}
+		if (depth == N)
+			return;
+
+		for (int i = depth; i < N; i++) {
+			if (!v[i]) {
+				v[i] = true;
+				arr.add(ig[i]);
+				bt(i);
+				v[i] = false;
+				arr.remove(arr.size() - 1);
+			}
+		}
+
+	}
+
+	static class Taste {
+		int s;
+		int b;
+
+		Taste(int s, int b) {
+			this.s = s;
+			this.b = b;
+		}
+	}
+}
