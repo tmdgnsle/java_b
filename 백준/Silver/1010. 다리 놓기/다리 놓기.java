7d533dc372999c28c1ee911889dba8fd8cf416c4
seunghoonlee <65535314+tmdgnsle@users.noreply.github.com>
2025-03-11 15:10:09 +0900

    [Silver V] Title: 다리 놓기, Time: 136 ms, Memory: 14464 KB -BaekjoonHub

---
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static int N, M;
+	static int[][] D;
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		D = new int[31][31];
+		for (int i = 0; i < 31; i++) {
+			D[i][0] = 1;
+			D[i][1] = 1;
+			D[i][i] = 1;
+		}
+
+		for (int i = 2; i < 31; i++) {
+			for (int j = 1; j < i; j++) {
+				D[i][j] = D[i - 1][j - 1] + D[i - 1][j];
+			}
+		}
+
+		int T = sc.nextInt();
+		for (int t = 1; t <= T; t++) {
+			N = sc.nextInt();
+			M = sc.nextInt();
+
+			System.out.println(D[M][N]);
+		}
+
+	}
+}
