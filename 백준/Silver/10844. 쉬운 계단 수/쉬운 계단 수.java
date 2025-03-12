66d1f7ca2d69bb25e7c21dd2ab1b3d9598eca407
seunghoonlee <65535314+tmdgnsle@users.noreply.github.com>
2025-03-12 17:17:31 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 96 ms, Memory: 12892 KB -BaekjoonHub

---
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static long mod = 1000000000;
+
+	public static void main(String[] args) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		long D[][] = new long[N + 1][11];
+		for (int i = 1; i <= 9; i++) {
+			D[1][i] = 1;
+		}
+
+		for (int i = 2; i <= N; i++) {
+			D[i][0] = D[i - 1][1];
+			D[i][9] = D[i - 1][8];
+			for (int j = 1; j <= 8; j++) {
+				D[i][j] = (D[i - 1][j - 1] + D[i - 1][j + 1]) % mod;
+			}
+		}
+		long sum = 0;
+		for (int i = 0; i < 10; i++) {
+			sum = (sum + D[N][i]) % mod;
+		}
+
+		System.out.println(sum);
+	}
+}
