d599058d990c020015fd574fd7fe3a511f0d6257
seunghoonlee <65535314+tmdgnsle@users.noreply.github.com>
2025-03-15 18:21:18 +0900

    [D3] Title: 햄버거 다이어트, Time: 190 ms, Memory: 26,624 KB -BaekjoonHub

---
+++ SWEA/D3/5215. 햄버거 다이어트/README.md
@@ -0,0 +1,15 @@
+# [D3] 햄버거 다이어트 - 5215 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AWT-lPB6dHUDFAVT) 
+
+### 성능 요약
+
+메모리: 26,624 KB, 시간: 190 ms, 코드길이: 1,615 Bytes
+
+### 제출 일자
+
+2025-03-15 18:21
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    static ArrayList<int[]> ingredient;
+    static int N, L;
+    static boolean[] v;
+    static int max;
+
+    public static void main(String[] args) throws Exception {
+       // System.setIn(new FileInputStream("res/input_5215.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        for (int t = 1; t <= T; t++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            N = Integer.parseInt(st.nextToken());
+            L = Integer.parseInt(st.nextToken());
+            max = Integer.MIN_VALUE;
+            ingredient = new ArrayList<>();
+            v = new boolean[N];
+            for (int i = 0; i < N; i++) {
+                st = new StringTokenizer(br.readLine());
+                int p1 = Integer.parseInt(st.nextToken());
+                int p2 = Integer.parseInt(st.nextToken());
+                ingredient.add(new int[]{p1, p2});
+            }
+
+            bt(0, 0, 0);
+
+            System.out.println("#" + t + " " + max);
+
+
+        }
+    }
+
+    static void bt(int start, int taste, int cal) {
+        for (int i = start; i < N; i++) {
+            if (!v[i]) {
+                int n_cal = cal + ingredient.get(i)[1];
+                int n_taste = taste + ingredient.get(i)[0];
+                if (n_cal <= L) {
+                    if (n_taste > max) max = n_taste;
+                    v[i] = true;
+                    bt(i, n_taste, n_cal);
+                    v[i] = false;
+                }
+            }
+        }
+
+    }
+
+}
