8f8480e0f1b16cf28bb38c59a077693427a3a75e
seunghoonlee <65535314+tmdgnsle@users.noreply.github.com>
2025-03-17 17:04:10 +0900

    [Silver III] Title: 계단 오르기, Time: 104 ms, Memory: 13384 KB -BaekjoonHub

---
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int[] stairs = new int[N + 1];
+		for (int i = 1; i <= N; i++) {
+			stairs[i] = sc.nextInt();
+		}
+
+		int[][] D = new int[N + 1][2];
+		D[1][0] = stairs[1];
+		D[1][1] = stairs[1];
+
+		for (int i = 2; i <= N; i++) {
+			D[i][0] = D[i - 1][1] + stairs[i];
+			D[i][1] = Math.max(D[i - 2][0], D[i - 2][1]) + stairs[i];
+		}
+
+		System.out.println(Math.max(D[N][0], D[N][1]));
+
+	}
+}
