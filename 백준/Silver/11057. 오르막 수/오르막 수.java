c0f31918431584ffc4e30af57f2a2ae72a2406ce
seunghoonlee <65535314+tmdgnsle@users.noreply.github.com>
2025-03-19 17:48:41 +0900

    [Silver I] Title: 오르막 수, Time: 92 ms, Memory: 12932 KB -BaekjoonHub

---
+++ 백준/Silver/11057. 오르막 수/오르막 수.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		if (n == 1)
+			System.out.println(10);
+		else {
+			int[][] dp = new int[n + 1][10];
+			for (int i = 0; i < 10; i++) {
+				dp[1][i] = 1;
+			}
+
+			for (int i = 2; i <= n; i++) {
+				for (int j = 0; j < 10; j++) {
+					int sum = 0;
+					for (int k = 0; k <= j; k++) {
+						sum += dp[i - 1][k] % 10007;
+					}
+					dp[i][j] = sum;
+				}
+
+			}
+
+			int result = 0;
+			for (int i = 0; i < 10; i++) {
+				result = (result + dp[n][i]) % 10007;
+			}
+
+			System.out.println(result);
+
+		}
+	}
+}
