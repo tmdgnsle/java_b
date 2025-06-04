234092113ec32307f92ba9efab2c73ab903bc955
seunghoonlee <65535314+tmdgnsle@users.noreply.github.com>
2025-06-05 01:34:29 +0900

    [level 2] Title: 조건에 맞는 사원 정보 조회하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/284527. 조건에 맞는 사원 정보 조회하기/조건에 맞는 사원 정보 조회하기.sql
@@ -0,0 +1,23 @@
+SELECT 
+    total_scores.SCORE,
+    E.EMP_NO,
+    E.EMP_NAME,
+    E.POSITION,
+    E.EMAIL
+FROM (
+    SELECT 
+        EMP_NO,
+        SUM(SCORE) AS SCORE
+    FROM HR_GRADE
+    WHERE YEAR = 2022
+    GROUP BY EMP_NO
+) total_scores
+JOIN HR_EMPLOYEES E ON total_scores.EMP_NO = E.EMP_NO
+WHERE total_scores.SCORE = (
+    SELECT SUM(SCORE)
+    FROM HR_GRADE
+    WHERE YEAR = 2022
+    GROUP BY EMP_NO
+    ORDER BY SUM(SCORE) DESC
+    LIMIT 1
+);
