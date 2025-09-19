507d49e004e73fa29b4cdc76bf8bbeaa34b340eb
seunghoonlee <65535314+tmdgnsle@users.noreply.github.com>
2025-09-19 20:59:03 +0900

    [Silver V] Title: 집합, Time: 1168 ms, Memory: 326400 KB -BaekjoonHub

---
+++ 백준/Silver/11723. 집합/집합.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+
+        int M = Integer.parseInt(br.readLine());
+        boolean[] S = new boolean[21];
+
+
+        for (int i = 0; i < M; i++) {
+            String[] input = br.readLine().split(" ");
+            String operation = input[0];
+
+            switch (operation) {
+                case "add":
+                    int addNum = Integer.parseInt(input[1]);
+                    S[addNum] = true;
+                    break;
+
+                case "remove":
+                    int removeNum = Integer.parseInt(input[1]);
+                    S[removeNum] = false;
+                    break;
+
+                case "check":
+                    int checkNum = Integer.parseInt(input[1]);
+                    if(S[checkNum]){
+                        sb.append(1 + "\n");
+                    }else {
+                        sb.append(0 + "\n");
+                    }
+                    break;
+
+                case "toggle":
+                    int toggleNum = Integer.parseInt(input[1]);
+                    S[toggleNum] = !S[toggleNum];
+                    break;
+
+                case "all":
+                    Arrays.fill(S, true);
+                    break;
+
+                case "empty":
+                    Arrays.fill(S, false);
+                    break;
+            }
+        }
+        System.out.print(sb);
+
+
+    }
+}
