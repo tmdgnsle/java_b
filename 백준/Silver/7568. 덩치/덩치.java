88de8d3ea66b73b7539536399c4e8715773db179
seunghoonlee <65535314+tmdgnsle@users.noreply.github.com>
2025-11-24 10:39:31 +0900

    [Silver V] Title: 덩치, Time: 64 ms, Memory: 11624 KB -BaekjoonHub

---
+++ 백준/Silver/7568. 덩치/덩치.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static Person[] p;
+
+    static class Person {
+        int weight;
+        int height;
+
+        public Person(int weight, int height) {
+            this.weight = weight;
+            this.height = height;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        p = new Person[N];
+        StringTokenizer st;
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int p1 = Integer.parseInt(st.nextToken());
+            int p2 = Integer.parseInt(st.nextToken());
+            p[i] = new Person(p1, p2);
+        }
+
+        int[] grades = new int[N];
+        for (int i = 0; i < N; i++) {
+            int grade = 1;
+            for (int j = 0; j < N; j++) {
+                if (i == j) continue;
+                if (p[i].weight < p[j].weight && p[i].height < p[j].height) grade++;
+            }
+            grades[i] = grade;
+        }
+
+        for (int i = 0; i < N; i++) {
+            System.out.print(grades[i] + " ");
+        }
+
+    }
+}
