f2c57193a67b6672ad4d1277a82c74ced47ca937
seunghoonlee <65535314+tmdgnsle@users.noreply.github.com>
2025-11-24 20:25:29 +0900

    [Silver II] Title: 수 이어 쓰기, Time: 80 ms, Memory: 13280 KB -BaekjoonHub

---
+++ 백준/Silver/1515. 수 이어 쓰기/수 이어 쓰기.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String S = br.readLine();
+
+        int idx = 0; // S에서 매칭되고 있는 위치
+        int num = 0; // 현재 이어 쓰고 있는 자연수
+
+        while (idx < S.length()) {
+            num++;
+            String cur = Integer.toString(num);
+
+            for (int i = 0; i < cur.length() && idx < S.length(); i++) {
+                if (cur.charAt(i) == S.charAt(idx)) {
+                    idx++; // 매칭되면 S의 다음 문자로 이동
+                }
+            }
+        }
+
+        System.out.println(num);
+    }
+}
