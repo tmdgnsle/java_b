93042148d1490df47934ca6c7371042f75bf457e
seunghoonlee <65535314+tmdgnsle@users.noreply.github.com>
2026-01-12 20:32:12 +0900

    [Silver V] Title: 임스와 함께하는 미니게임, Time: 196 ms, Memory: 25236 KB -BaekjoonHub

---
+++ 백준/Silver/25757. 임스와 함께하는 미니게임/임스와 함께하는 미니게임.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N;
+        char G;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        G = st.nextToken().charAt(0);
+
+        HashSet<String> set = new HashSet<>();
+        for(int i = 0; i<N; i++){
+            set.add(br.readLine());
+        }
+
+        int answer = 0;
+
+        int total = set.size();
+
+        if(G == 'Y'){
+            answer = total;
+        }else if(G == 'F'){
+            answer = total / 2;
+        }else if(G == 'O'){
+            answer = total / 3;
+        }
+
+        System.out.println(answer);
+
+    }
+}
