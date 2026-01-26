4c2d7ee6ee09d92761dd6633c6bba1efc87b0670
seunghoonlee <65535314+tmdgnsle@users.noreply.github.com>
2026-01-26 21:59:25 +0900

    [Silver III] Title: 햄버거 분배, Time: 76 ms, Memory: 11984 KB -BaekjoonHub

---
+++ 백준/Silver/19941. 햄버거 분배/햄버거 분배.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        char[] table = new char[N];
+        String str = br.readLine();
+        for(int i = 0; i<N; i++){
+            table[i] = str.charAt(i);
+        }
+        boolean[] can = new boolean[N];
+
+        int answer = 0;
+        for(int i = 0; i<N; i++){
+            if(table[i] == 'P'){
+                for(int j = i - K; j<= i + K; j++){
+                    if(j < 0 || j >= N) continue;
+
+                    if(table[j] == 'H' && !can[j]){
+                        can[j] = true;
+                        answer++;
+                        break;
+                    }
+
+                }
+            }
+        }
+
+        System.out.println(answer);
+
+
+    }
+}
