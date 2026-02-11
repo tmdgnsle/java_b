9b8f0d4ea9dbc1d78ff94c43c8aedb68a3bb377a
seunghoonlee <65535314+tmdgnsle@users.noreply.github.com>
2026-02-11 16:17:48 +0900

    [Gold V] Title: 문자열 게임 2, Time: 2780 ms, Memory: 20436 KB -BaekjoonHub

---
+++ 백준/Gold/20437. 문자열 게임 2/문자열 게임 2.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        for (int t = 0; t < T; t++) {
+            String str = br.readLine();
+            int K = Integer.parseInt(br.readLine());
+
+            if (K == 1) {
+                System.out.println("1 1");
+                continue;
+            }
+
+            int[] num = new int[str.length()];
+            for(int i = 0; i<str.length(); i++){
+                char first = str.charAt(i);
+                int count = 1;
+                for(int j = i+1; j<str.length(); j++) {
+                    char second = str.charAt(j);
+                    if (first == second) {
+                        count++;
+                        if (count == K) {
+                            num[i] = j - i + 1;
+                            break;
+                        }
+                    }
+                }
+            }
+            int min = Integer.MAX_VALUE;
+            int max = Integer.MIN_VALUE;
+            for(int i = 0; i<str.length(); i++){
+                if(num[i] == 0) continue;
+                min = Math.min(min, num[i]);
+                max = Math.max(max, num[i]);
+            }
+
+            if(min == Integer.MAX_VALUE || max == Integer.MIN_VALUE) System.out.println(-1);
+            else System.out.println(min + " " + max);
+        }
+    }
+}
