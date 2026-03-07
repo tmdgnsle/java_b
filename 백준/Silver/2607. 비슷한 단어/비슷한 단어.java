1897d41d4eb464cc6055f370eb9789c929884c20
seunghoonlee <65535314+tmdgnsle@users.noreply.github.com>
2026-03-07 18:46:54 +0900

    [Silver II] Title: 비슷한 단어, Time: 60 ms, Memory: 11500 KB -BaekjoonHub

---
+++ 백준/Silver/2607. 비슷한 단어/비슷한 단어.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int answer = 0;
+
+
+        int[] alpha = new int[26];
+        String str = br.readLine();
+        for(char c: str.toCharArray()){
+            int idx = c - 'A';
+            alpha[idx]++;
+        }
+        int length = str.length();
+
+        for(int i = 0; i<N-1; i++){
+            int[] targetAlpha = new int[26];
+            String target = br.readLine();
+            int targetLength = target.length();
+            if(Math.abs(length - targetLength) >= 2) continue;
+            for(char c: target.toCharArray()){
+                int idx = c - 'A';
+                targetAlpha[idx]++;
+            }
+
+            int diff = 0;
+
+            for(int j = 0; j<26; j++){
+                diff += Math.abs(targetAlpha[j] - alpha[j]);
+            }
+
+            if(diff <= 2) answer++;
+
+        }
+        System.out.println(answer);
+    }
+}
