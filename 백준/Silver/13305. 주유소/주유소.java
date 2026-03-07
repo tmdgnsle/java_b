f44ecf52ad0757a73d30c403319be5203c2100c4
seunghoonlee <65535314+tmdgnsle@users.noreply.github.com>
2026-03-07 18:23:16 +0900

    [Silver III] Title: 주유소, Time: 248 ms, Memory: 31244 KB, Score: 58 point -BaekjoonHub

---
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] dist = new int[N-1];
+        int[] prices = new int[N];
+        long answer = 0;
+
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 0; i<N-1; i++){
+            dist[i] = Integer.parseInt(st.nextToken());
+        }
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i<N; i++){
+            int price = Integer.parseInt(st.nextToken());
+            if(i == 0) prices[i] = price;
+            else prices[i] = Math.min(prices[i-1], price);
+        }
+
+        for(int i = 0; i<N-1; i++){
+            answer += prices[i] * dist[i];
+        }
+
+        System.out.println(answer);
+
+
+    }
+}
