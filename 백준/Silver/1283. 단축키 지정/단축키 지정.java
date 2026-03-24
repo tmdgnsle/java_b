5777ac15b37bd7308b0730a916660127f6ff4b71
seunghoonlee <65535314+tmdgnsle@users.noreply.github.com>
2026-03-24 15:34:24 +0900

    [Silver I] Title: 단축키 지정, Time: 64 ms, Memory: 11604 KB -BaekjoonHub

---
+++ 백준/Silver/1283. 단축키 지정/단축키 지정.java
@@ -0,0 +1,69 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        boolean[] used = new boolean[26];
+        StringBuilder sb = new StringBuilder();
+
+        for (int t = 0; t < n; t++) {
+            String line = br.readLine();
+            int idx = -1;
+
+            StringTokenizer st = new StringTokenizer(line);
+            int pos = 0;
+
+            while (st.hasMoreTokens()) {
+                String word = st.nextToken();
+                while (pos < line.length() && line.charAt(pos) == ' ') {
+                    pos++;
+                }
+
+                char c = line.charAt(pos);
+                int alpha = Character.toLowerCase(c) - 'a';
+
+                if (!used[alpha]) {
+                    used[alpha] = true;
+                    idx = pos;
+                    break;
+                }
+
+                pos += word.length();
+            }
+
+            if (idx == -1) {
+                for (int i = 0; i < line.length(); i++) {
+                    char c = line.charAt(i);
+
+                    if (Character.isAlphabetic(c)) {
+                        int alpha = Character.toLowerCase(c) - 'a';
+                        if (!used[alpha]) {
+                            used[alpha] = true;
+                            idx = i;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            if (idx == -1) {
+                sb.append(line).append("\n");
+            } else {
+                sb.append(line, 0, idx)
+                        .append('[')
+                        .append(line.charAt(idx))
+                        .append(']')
+                        .append(line.substring(idx + 1))
+                        .append("\n");
+            }
+
+        }
+
+        System.out.println(sb);
+
+
+    }
+}
