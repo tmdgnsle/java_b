1252db0e279de9a66c9296d0677ffcf65c0cc7ff
seunghoonlee <65535314+tmdgnsle@users.noreply.github.com>
2026-04-03 01:52:29 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 80 ms, Memory: 12224 KB -BaekjoonHub

---
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.kt
@@ -0,0 +1,15 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.StringTokenizer
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val st = StringTokenizer(br.readLine())
+    val a = st.nextToken().toLong()
+    val b = st.nextToken().toLong()
+
+    println(gcd(a, b))
+    println(lcm(a, b))
+}
+fun gcd(a: Long, b: Long): Long = if (b == 0L) a else gcd(b, a % b)
+fun lcm(a: Long, b: Long): Long = a / gcd(a, b) * b
