cce6a92e045e4f59a0f82182c47037a6c1bf67c0
seunghoonlee <65535314+tmdgnsle@users.noreply.github.com>
2026-04-03 02:06:49 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 76 ms, Memory: 12224 KB -BaekjoonHub

---
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.kt
@@ -0,0 +1,23 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.StringTokenizer
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val st = StringTokenizer(br.readLine())
+
+    val a = st.nextToken().toInt()
+    val b = st.nextToken().toInt()
+    val v = st.nextToken().toInt()
+
+    val daily = a - b
+    val beforeLast = v - a
+
+    val answer = if (beforeLast <= 0) {
+        1
+    } else {
+        (beforeLast + daily - 1) / daily + 1 
+    }
+
+    println(answer)
+}
