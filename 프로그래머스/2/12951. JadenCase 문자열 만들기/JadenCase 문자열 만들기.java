22e241abd71736c386edd632eea14fa592b3239a
seunghoonlee <65535314+tmdgnsle@users.noreply.github.com>
2026-07-22 17:37:03 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.05 ms, Memory: 87.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String solution(String s) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if (i == 0 || s.charAt(i - 1) == ' ') {
+                sb.append(Character.toUpperCase(c));
+            } else {
+                sb.append(Character.toLowerCase(c));
+            }
+        }
+
+        return sb.toString();
+    }
+}
